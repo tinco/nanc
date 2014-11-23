14f16a37e498a78de28c40b6aad5f413a5993d5e
Tinco Andringa <mail@tinco.nl>
2014-11-23 02:38:17 +0100

    large amount of shitty code that almost parses a declaration

--- Nanc/AST.hs
+++ Nanc/AST.hs
@@ -2,18 +2,37 @@ module Nanc.AST where
 
 import Language.C
 
+import Debug.Trace
+
 data StorageSpec = Auto | Register | Static | Extern | Typedef | Thread | NoStorageSpec
+	deriving (Show)
 
-data SimpleType = Int |
-	LongSignedInt | LongUnsignedInt |
-	ShortSignedInt | ShortUnsignedInt |
-	LongDouble | Double |
-	SignedChar | Char |
-	Void | Float
+data SimpleType = 
+	Char |
+	SignedChar |
+	UnsignedChar |
+	SignedShortInt |
+	UnsignedShortInt |
+	SignedInt |
+	UnsignedInt |
+	SignedLongInt |
+	UnsignedLongInt |
+	SignedLongLongInt |
+	UnsignedLongLongInt |
+	Float |
+	Double |
+	LongDouble |
+	Bool |
+	Void
+	deriving (Show)
 
-data ComplexType = CSU CStructUnion | E CEnum | TD Ident 
+data ComplexType = CSU CStructUnion | E CEnum | TD Ident | TOE CExpr | TOT CDecl deriving (Show)
 
-data TypeSpec = CT ComplexType | ST SimpleType
+data TypeSpec = CT ComplexType | ST SimpleType | NoTypeSpec deriving (Show)
+
+isNoTypeSpec :: TypeSpec -> Bool
+isNoTypeSpec NoTypeSpec = True
+isNoTypeSpec _ = False
 
 data TypeQualifiers = TypeQualifiers {
 	typeIsVolatile :: Bool,
@@ -37,7 +56,7 @@ data DeclarationSpecs = DeclarationSpecs {
 }
 
 defaultDeclarationSpec :: DeclarationSpecs
-defaultDeclarationSpec = DeclarationSpecs NoStorageSpec (ST Int) defaultTypeQualifiers [] [] [] [] []
+defaultDeclarationSpec = DeclarationSpecs NoStorageSpec NoTypeSpec defaultTypeQualifiers [] [] [] [] []
 
 buildDeclarationSpecs :: [CDeclSpec] -> DeclarationSpecs
 buildDeclarationSpecs specs = build defaultDeclarationSpec specs
@@ -60,11 +79,64 @@ buildDeclarationSpecs specs = build defaultDeclarationSpec specs
 			parse (CTypedef n) = (Typedef,n)
 			parse (CThread n) = (Thread,n)
 
-		updateTypeSpec ds spec rest = ds { 
-			declType = declType',
-			declTypeNodes = n : (declTypeNodes ds) 
-		}
+		updateTypeSpec :: DeclarationSpecs -> CTypeSpec -> [CDeclSpec] -> DeclarationSpecs
+		updateTypeSpec ds spec rest
+			| hasSpec = ds
+			| otherwise = ds { 
+				declType = declType',
+				declTypeNodes = n : (declTypeNodes ds) 
+			}
 			where
-				(declType', n) = undefined
+				specs = spec : (filterTypeSpecs rest)
+				filterTypeSpecs :: [CDeclSpec] -> [CTypeSpec]
+				filterTypeSpecs [] = []
+				filterTypeSpecs ((CTypeSpec ts):rs) = ts : filterTypeSpecs rs
+				filterTypeSpecs _ = []
+				hasSpec = isNoTypeSpec $ declType ds
+				(declType', n) = parse spec (filterTypeSpecs rest) 
+				-- we willen weten of er een int is in te typespec
+				-- als die er is dan willen we weten of er ook een
+				-- long, signed, of int is.
+				--
+				parse :: CTypeSpec -> [CTypeSpec] -> (TypeSpec, NodeInfo)
+				parse (CVoidType n) _ = (ST Void, n)
+				parse (CFloatType n) _ = (ST Float, n)
+				parse (CBoolType n) _ = (ST Bool, n)
+				parse (CComplexType n) _ = trace ("What the hell is a ComplexType? " ++ (show n)) undefined
+				parse (CSUType u n) _ = (CT (CSU u), n)
+				parse (CEnumType e n) _ = (CT (E e), n)
+				parse (CTypeDef i n) _ = (CT (TD i), n)
+				parse (CTypeOfExpr e n) _ = (CT (TOE e), n)
+				parse (CTypeOfType d n) _ = (CT (TOT d), n)
+				parse (CCharType n) _ 
+					| hasSigned = (ST SignedChar, n)
+					| hasUnsigned = (ST UnsignedChar, n)
+					| otherwise = (ST Char, n)
+				parse (CDoubleType n) _
+					| hasLong = (ST LongDouble, n)
+					| otherwise = (ST Double, n)
+				parse (CIntType n) _
+					| hasTwoLong && hasUnsigned = (ST UnsignedLongLongInt, n)
+					| hasTwoLong = (ST SignedLongLongInt, n)
+					| hasLong && hasUnsigned = (ST UnsignedLongInt, n)
+					| hasLong = (ST SignedLongLongInt, n)
+					| hasShort && hasUnsigned = (ST UnsignedShortInt, n)
+					| hasShort = (ST SignedShortInt, n)
+					| hasUnsigned = (ST UnsignedInt, n)
+					| otherwise = (ST SignedInt, n)
+
+				-- think about just long, just short, long long.. etc?
+				parse (CShortType _) (d:r) = parse d r
+				parse (CSignedType _) (d:r) = parse d r
+				parse (CUnsigType _) (d:r) = parse d r
+				parse (CLongType _) (d:r) = parse d r
+
+				hasSigned = undefined
+				hasUnsigned = undefined
+				hasLong = undefined
+				hasTwoLong = undefined
+				hasShort = undefined
+
+
 
 		updateTypeQual ds qual = undefined
