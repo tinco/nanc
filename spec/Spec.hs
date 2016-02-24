3a1236ac7d6623f6101cd2c0983e457c69fed2c6
Tinco Andringa <mail@tinco.nl>
2016-02-24 01:30:11 +0100

    generate IR for all execute tests

--- spec/Spec.hs
+++ spec/Spec.hs
@@ -3,7 +3,10 @@ import Test.Hspec
 import Nanc.Compiler
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+
 import Data.Either
+import Data.List
+import System.Directory
 
 main :: IO ()
 main = hspec $ do
@@ -16,6 +19,7 @@ main = hspec $ do
 
 	describe "Nanc.Compiler.transformToLLVM" $ do
 		it "Transforms globals to llvm ast" $ do
+			pendingWith "This doesn't do anything.."
 			src <- testFile "0002-global1"
 			let (Right ast) = parse "global" src
 			let transformed = transformToLLVM ast
@@ -36,5 +40,26 @@ main = hspec $ do
 			ir <- compileSrcToIR "0002-global2.c" src
 			ir `shouldSatisfy` (/= T.empty)
 
+		describe "Transforms all execute tests to llvm ast" $ do
+			let runTest (name, src) = do
+				it ("Transforms " ++ name) $ do
+					let (Right ast) = parse "global" src
+					let transformed = transformToLLVM ast
+					ir <- generateIR transformed
+					ir `shouldSatisfy` (/= T.empty)
+
+			tests <- runIO $ testFiles "execute"
+			mapM_ runTest tests
+
 testFile :: FilePath -> IO T.Text
 testFile name = TIO.readFile $ "spec/spec_support/execute/" ++ name ++ ".c"
+
+testFiles :: FilePath -> IO [(FilePath, T.Text)] 
+testFiles subdir = do
+	let dir = "spec/spec_support/" ++ subdir ++ "/"
+	files <- getDirectoryContents dir
+	let sourceFiles = filter (isInfixOf ".c") files
+	let sourceFilesWithPaths = map ((++) dir) sourceFiles
+	sources <- mapM TIO.readFile sourceFilesWithPaths
+	let result = zip sourceFiles sources
+	return result
